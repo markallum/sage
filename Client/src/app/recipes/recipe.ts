18192b94f719117051eb6d7941d24d88954c25a0
Mark Allum <mark.allum@hotmail.co.uk>
2019-11-14 07:18:21 +0000

    Created base recipe model class

---
+++ Client/src/app/recipes/recipe.ts
@@ -0,0 +1,10 @@
+export class Recipe {
+    title: string;
+    dateCreated: string;
+    dateUpdated: string;
+    author: string;
+    prepTimeMinutes: number;
+    cookTimeMinutes: number;
+    method: string;
+    ingredients: string[];
+}
